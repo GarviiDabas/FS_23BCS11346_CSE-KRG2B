3cf539c0992ef178c7169e72ec49f615b408796f
GarviiDabas <garvidabas177@gmail.com>
2025-11-10 12:37:15 +0530

    working

--- Project/Virtual_Queue_System/virtual-queue-frontend/src/services/apiClient.js
+++ Project/Virtual_Queue_System/virtual-queue-frontend/src/services/apiClient.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // 1. Get the API URL from environment variables (for Vercel)
 //    and provide a fallback for local development (http://localhost:8080)
-const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api` || 'http://localhost:8080/api';
+// const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api` || 'http://localhost:8080/api';
+const API_BASE_URL = 'https://virtual-queue-system-backnd.onrender.com/api'
 // 2. Create a configured instance of axios
 const apiClient = axios.create({
   baseURL: API_BASE_URL, // Use the variable here
